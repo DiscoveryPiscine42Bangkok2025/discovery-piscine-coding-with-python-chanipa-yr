f78a35f84db5f6ac62470724d4c85f734b4085c7
chanipa-yr <68030056@kmitl.ac.th>
2025-09-17 14:24:14 +0000

    cell02/ex01

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+number = float(input('pls enter your number'))
+
+if number < 0:
+    print('this number is negative')
+elif number > 0:
+    print('this number is positive.')
+else:
+    print('thos number is both positive and negative')
